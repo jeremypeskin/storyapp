f33f2119eb61a57cc9e66fcc5d29d75f36a315e6
Jeremy Peskin <jeremy.p.peskin@gmail.com>
2018-04-29 09:12:52 -0400

    Successfully made helloworld component render as list

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" app>
-      <PageSettings :font="fontFamily" @fontWasEdited="fontFamily = $event" :inputx="input" @inputWasEdited="input = $event"></PageSettings>
-      <ImageSettings :illustrationx="illustration" @illustrationWasEdited="illustration=$event"></ImageSettings>
+      <!--<PageSettings :font="fontFamily" @fontWasEdited="fontFamily = $event" :inputx="input" @inputWasEdited="input = $event"></PageSettings>-->
+      <!--<ImageSettings :illustrationx="illustration" @illustrationWasEdited="illustration=$event"></ImageSettings>-->
     </v-navigation-drawer>
     <v-toolbar app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
     </v-toolbar>
     <v-content>
-      <HelloWorld :font="fontFamily" :inputx="input" :illustrationx="illustration"></HelloWorld>
+      <HelloWorld v-for="page in pages" :font="fontFamily" :inputx="page.input" :illustrationx="page.illustration"></HelloWorld>
     </v-content>
   </v-app>
 </template>
@@ -16,23 +16,25 @@
 <script>
 
 import HelloWorld from './components/HelloWorld'
-import PageSettings from './components/PageSettings'
-import ImageSettings from './components/ImageSettings'
+//import PageSettings from './components/PageSettings'
+//import ImageSettings from './components/ImageSettings'
 
 export default {
   data () {
     return {
+      pages: [
+        {input: 'Write your verse, No need to rehearse.',
+        illustration: 'forest.png'}
+      ],
       drawer: false,
       fontFamily: 'blackAndWhite',
-      input: 'Write your verse, No need to rehearse.',
-      illustration: 'forest.png'
     }
   },
   name: 'App',
   components: {
     HelloWorld,
-    PageSettings,
-    ImageSettings
+    //PageSettings,
+    //ImageSettings
   }
 }
 </script>
